2502c8ea1d83b95879cca7d7bd39e15d215daad5
enrique tremino <enrique.tremino@neogeo.com>
2019-02-13 18:14:18 +0100

    Add project files.

---
+++ WebApplication3/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static int MessageCount = 0;
+
+        public void Send(string user, string message)
+        {
+            MessageCount++;
+
+            Clients.All.addNewMessageToPage(user, MessageCount.ToString() + ") " + message);
+
+        }
+    }
+
+}
---
+++ WebApplication3/Providers/ApplicationOAuthProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.OAuth;
+
+namespace WebApplication3.Providers
+{
+    public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        private readonly string _publicClientId;
+
+        public ApplicationOAuthProvider(string publicClientId)
+        {
+            if (publicClientId == null)
+            {
+                throw new ArgumentNullException("publicClientId");
+            }
+
+            _publicClientId = publicClientId;
+        }
+
+        public override Task ValidateClientRedirectUri(OAuthValidateClientRedirectUriContext context)
+        {
+            if (context.ClientId == _publicClientId)
+            {
+                // ET: Damit die Anwengung nicht im RootVerzeichnigs laufen muss, wurde "/" mit ApplicationPath ausgetauscht.
+                // So wird dir richtige URL zurückgegeben 
+                Uri expectedRootUri = new Uri(context.Request.Uri, System.Web.HttpContext.Current.Request.ApplicationPath);
+
+
+                if (expectedRootUri.AbsoluteUri == context.RedirectUri)
+                {
+                    context.Validated();
+                }
+                else if (context.ClientId == "web")
+                {
+                    var expectedUri = new Uri(context.Request.Uri, System.Web.HttpContext.Current.Request.ApplicationPath);
+                    context.Validated(expectedUri.AbsoluteUri);
+                }
+            }
+
+            return Task.FromResult<object>(null);
+        }
+    }
+}
